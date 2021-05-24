be87bd752c8fab63894a60251a781cea7c4e3981
Your Name <you@example.com>
2021-05-24 11:43:11 +0530

    first commit

---
+++ app/src/main/java/com/example/crud/UserModel.java
@@ -0,0 +1,34 @@
+package com.example.crud;
+
+import java.io.Serializable;
+
+public class UserModel implements Serializable {
+
+    private String name, model;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setHobby(String model) {
+        this.model = model;
+    }
+}
+
